05f1246bf347f223c5d72412f75121688d25ae50
unknown <csigerkrisztian@gmail.com>
2026-05-14 11:19:33 +0200

    Reszponzívvá tenni a képet a cardon belül

--- src/pages/Fishes.jsx
+++ src/pages/Fishes.jsx
@@ -25,7 +25,7 @@ function Fishes() {
                                 <div className='card-header'>Név: {fish.nev}</div>
                                 <div className="card-body">
                                     <p>Faj: {fish.faj}</p>
-                                    <img className='w-50' src={`data:image/jpeg;base64,${fish.kep}`} alt={fish.nev}/>
+                                    <img className='img-fluid' src={`data:image/jpeg;base64,${fish.kep}`} alt={fish.nev}/>
                                 </div>
                                 <div className='card-footer'>
                                     <Link to={`/hal/${fish.id}`} className="btn btn-primary">További adatok<i className="bi-text-paragraph"></i></Link>
